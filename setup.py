2b9b730b490b17a1e26331dce384f0ca3bb9a536
Danny Mcwaves <dannymcwaves96@gmail.com>
2016-12-08 07:30:36 +0000

    adding a new setup file

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name="packet",
+    version="1.0.0",
+    packages=["packet", "packet/format"],
+    author="Danny mcwaves",
+    author_email="dannymcwaves96@gmail.com",
+    url="https://github.com/DannyMcwaves/packet",
+    description="crafting custom packets with python.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
+
